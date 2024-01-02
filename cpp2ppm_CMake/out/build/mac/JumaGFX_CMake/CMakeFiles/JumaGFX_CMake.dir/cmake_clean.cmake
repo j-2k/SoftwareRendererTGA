11ab1c69c79de2b3da413f0553eb8ffc39c3b362
Juma Al Remeithi <juma_ad@hotmail.com>
2024-01-02 09:22:02 +0400

    Mac Cmake Test

---
+++ cpp2ppm_CMake/out/build/mac/JumaGFX_CMake/CMakeFiles/JumaGFX_CMake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JumaGFX_CMake.dir/JumaGFX_CMake.cpp.o"
+  "CMakeFiles/JumaGFX_CMake.dir/JumaGFX_CMake.cpp.o.d"
+  "JumaGFX_CMake"
+  "JumaGFX_CMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JumaGFX_CMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp2ppm_CMake/out/build/mac/JumaGFX_CMake/CMakeFiles/JumaGFX_CMake.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for JumaGFX_CMake.
+# This may be replaced when dependencies are built.
