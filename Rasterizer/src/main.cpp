d36f143052d33acbda3b28d94edb0d597683d1ce
Juma Al Remeithi <juma_ad@hotmail.com>
2024-01-04 08:56:29 +0400

    Bresenham's Line Algorithim Optimized

--- Rasterizer/src/main.cpp
+++ Rasterizer/src/main.cpp
@@ -5,24 +5,45 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
 
-
+//bresenham line algorithm
 void line2p(int x1,int y1, int x2, int y2, TGAImage &image, TGAColor color)
 {
-	if(x1>x2 || y1 > y2)
-	{
+	bool steep = false;
+	if(std::abs(x1-x2) < std::abs(y1-y2)){
+		std::swap(x1,y1);
+		std::swap(x2,y2);
+		steep = true;
+	}
+	
+	if(x1>x2){				// || y1 > y2){
 		std::swap(x1,x2);
 		std::swap(y1,y2);
 	}
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+	int derror2 = std::abs(dy)*2;
+	float error2 = 0;
+
+	int y = y1;
 	
-	for (int px = x1; px < x2; px++)
-	{
+	for (int px = x1; px <= x2; px++){
 		float mag = (float)(px - x1) / (float)(x2 - x1);
-		//int xdir = x1 + (x2 - x1) * i;
-		
 		int py = (y1*(1.-mag)) + (y2*mag);
+		if(steep)
+			{image.set(py,px,color);} //if steep (transposed) then return to normal
+		else
+			{image.set(px,py,color);} //normal
+
+		error2 += derror2;
+		if (error2 > dx){
+			y += (y2>y1 ? 1:-1);
+			error2 -= dx*2;
+		}
 		
-		image.set(px,py,color);
 	}
 }
 
@@ -42,7 +63,7 @@ int main(int argc, char** argv)
 
 	line2p(13, 20, 80, 40, image, white); 
 	line2p(20, 13, 40, 80, image, red); 
-	line2p(90, 50, 23, 30, image, red);
+	line2p(90, 50, 23, 30, image, green);
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("../images/output.tga");
