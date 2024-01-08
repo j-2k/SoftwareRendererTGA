324c47052ab22fa30bc817b7e88edd01f72c573e
Juma Al Remeithi <juma_ad@hotmail.com>
2024-01-08 14:14:15 +0400

    Triangles Stage

--- Rasterizer/src/main.cpp
+++ Rasterizer/src/main.cpp
@@ -10,9 +10,13 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
+const TGAColor yellow = TGAColor(255, 255, 0, 255);
+const TGAColor purple = TGAColor(255, 0, 255, 255);
+
 Model *model = NULL;
-const int width  = 1000;
-const int height = 1000;
+const int width  = 200;
+const int height = 200;
 
 //bresenham line algorithm
 void line2p(int x1,int y1, int x2, int y2, TGAImage &image, TGAColor color)
@@ -54,6 +58,35 @@ void line2p(int x1,int y1, int x2, int y2, TGAImage &image, TGAColor color)
 	}
 }
 
+void line2p(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        steep = true;
+    }
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
+    }
+
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+    }
+}
+
+//Triangle
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line2p(t0, t1, image, color);
+    line2p(t1, t2, image, color);
+    line2p(t2, t0, image, color);
+}
+
 int main(int argc, char** argv)
 {
 	//terminal to run > "cmake .. && make && ./rasterizer" in 1 command
@@ -74,9 +107,8 @@ int main(int argc, char** argv)
 	}
 	*/
 
-	
-	
 	model = new Model("../../models/duoranger.obj");
+	/* Wireframe Rendering
 	for (int i=0; i<model->nfaces(); i++) { 
     std::vector<int> face = model->face(i); 
     for (int j=0; j<3; j++) { 
@@ -91,7 +123,17 @@ int main(int argc, char** argv)
         line2p(x0, y0, x1, y1, image, color); 
 		} 
 	}
+	*/
 	
+	/*Triangle Rendering*/
+    Vec2i t0[3] = {Vec2i(50, 20),   Vec2i(100, 10),  Vec2i(20, 80)};
+    Vec2i t1[3] = {Vec2i(150, 50),  Vec2i(20, 50),   Vec2i(10, 10)};
+    Vec2i t2[3] = {Vec2i(199, 199), Vec2i(120, 160), Vec2i(5, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, purple);
+    triangle(t1[0], t1[1], t1[2], image, blue);
+    triangle(t2[0], t2[1], t2[2], image, yellow);
+
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	std::string rstr = std::to_string(run);
