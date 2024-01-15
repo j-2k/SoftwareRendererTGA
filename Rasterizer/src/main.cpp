f4e768a22299b8bc0c1d42ca8d295294785f04cc
Juma Al Remeithi <juma_ad@hotmail.com>
2024-01-15 14:56:57 +0400

    Learning Prespective Projection

--- Rasterizer/src/main.cpp
+++ Rasterizer/src/main.cpp
@@ -7,12 +7,74 @@ const TGAColor blue  = TGAColor(0,   0,   255, 255);
 const TGAColor yellow = TGAColor(255, 255, 0, 255);
 const TGAColor purple = TGAColor(255, 0, 255, 255);
 
-const Vec3f light_direction(1,-1,-1); //Vec3f(-1,-1,-1);
-
+//const Vec3f light_direction(1,-1,-1); //Vec3f(-1,-1,-1);
+const Vec3f light_direction(0,0,-1);
 
 Model *model = NULL;
 const int width  = 1000;
 const int height = 1000;
+const int depth = 255;
+
+Vec3f camera(0,0,7);
+int *zbuffer = NULL;
+
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);//(x/w,y/w,z/w) divide by w to get the normalized vector
+}
+
+Matrix v2m(Vec3f v) {	//converts a vector to a matrix
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {	//converts the viewport to a matrix
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;		//moves the viewport to the center of the screen
+    m[1][3] = y+h/2.f;		//moves the viewport to the center of the screen
+    m[2][3] = depth/2.f;	//what is happening here? this is scaling the viewport to the depth of the screen
+
+    m[0][0] = w/2.f;	//scale the viewport
+    m[1][1] = h/2.f; 	//scale the viewport
+    m[2][2] = depth/2.f;//scale the viewport
+    return m;
+}
+
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, TGAImage &image, float intensity, int *zbuffer) {
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+    if (t0.y>t1.y) { std::swap(t0, t1); std::swap(uv0, uv1); }
+    if (t0.y>t2.y) { std::swap(t0, t2); std::swap(uv0, uv2); }
+    if (t1.y>t2.y) { std::swap(t1, t2); std::swap(uv1, uv2); }
+
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+        Vec3i A   =               t0  + Vec3f(t2-t0  )*alpha;
+        Vec3i B   = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
+        Vec2i uvA =               uv0 +      (uv2-uv0)*alpha;
+        Vec2i uvB = second_half ? uv1 +      (uv2-uv1)*beta : uv0 +      (uv1-uv0)*beta;
+        if (A.x>B.x) { std::swap(A, B); std::swap(uvA, uvB); }
+        for (int j=A.x; j<=B.x; j++) {
+            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+            Vec3i   P = Vec3f(A) + Vec3f(B-A)*phi;
+            Vec2i uvP =     uvA +   (uvB-uvA)*phi;
+            int idx = P.x+P.y*width;
+            if (zbuffer[idx]<P.z) {
+                zbuffer[idx] = P.z;
+                TGAColor color = model->diffuse(uvP);
+                image.set(P.x, P.y, TGAColor(color.r*intensity, color.g*intensity, color.b*intensity));
+            }
+        }
+    }
+}
+
 
 int main(int argc, char** argv)
 {
@@ -34,7 +96,9 @@ int main(int argc, char** argv)
 	}
 	*/
 
-	model = new Model("../../models/african_head.obj");
+	model = new Model("../../models/duoranger.obj");
+	//model = new Model("../../models/african_head.obj");
+
 	/* Obj Wireframe Rendering Lines Old
 	for (int i=0; i<model->nfaces(); i++) { 
     std::vector<int> face = model->face(i); 
@@ -124,8 +188,7 @@ int main(int argc, char** argv)
     line2p(Vec2i(10, 10), Vec2i(790, 10), image, white);
 	*/
 
-	
-	/*Z Buffer implementation WITH Obj Triangle Rendering & Lighting */
+	/*Z Buffer implementation WITH Obj Triangle Rendering & Lighting 
 	float *zbuffer = new float[width*height];
     for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
@@ -137,23 +200,77 @@ int main(int argc, char** argv)
 			pts[i] = world2screen(model->vert(face[i]), width, height);
 			world_coords[i] = (model->vert(face[i]));
 		}
-		Vec3f normal = cross((world_coords[2]-world_coords[0]),(world_coords[1]-world_coords[0]));
+		Vec3f normal = ((world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]));
 		normal.normalize();
 		Vec3f lightDir = light_direction;
 		lightDir.normalize();
-		int lightIntensity = (int)clampminmax(((normal*lightDir) * 255.f),0.f,255.f);
+		float dot01 = clampminmax(normal*lightDir,0.f,1.f);
+		int lightIntensity = (int)clampminmax((dot01 * 255.f),0.f,255.f);
 		triangle(pts, zbuffer, image, TGAColor(lightIntensity, lightIntensity, lightIntensity, 255));
 		//triangle(pts, zbuffer, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
+	*/
+
+	/*Prespective Projection*/
+
+
+	zbuffer = new int[width*height];
+    for (int i=0; i<width*height; i++) {
+    zbuffer[i] = std::numeric_limits<int>::min();
+    }
+
+    { // draw the model
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+        Projection[3][2] = -1.f/camera.z;
+
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i=0; i<model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            for (int j=0; j<3; j++) {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] =  m2v(ViewPort*Projection*v2m(v));
+                world_coords[j]  = v;
+            }
+            Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+            n.normalize();
+            float intensity = n*light_direction;
+            if (intensity>0) {
+                Vec2i uv[3];
+                for (int k=0; k<3; k++) {
+                    uv[k] = model->uv(i, k);
+                }
+                triangle(screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], image, intensity, zbuffer);
+            }
+        }
+
+		image.flip_vertically(); //origin at the left bottom corner of the image
+		std::string rstr = std::to_string(run);
+		std::string output = "../../images/output" + rstr + ".tga";
+
+		image.write_tga_file(output.c_str());
+    }
+
+
 	
-
-
-	image.flip_vertically(); //origin at the left bottom corner of the image
-	std::string rstr = std::to_string(run);
-	std::string output = "../../images/output" + rstr + ".tga";
-
-	image.write_tga_file(output.c_str());
-	delete model;
+	{
+	    // dump z-buffer (debugging purposes only)
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i=0; i<width; i++) {
+            for (int j=0; j<height; j++) {
+                zbimage.set(i, j, TGAColor(zbuffer[i+j*width], 1));
+            }
+        }
+        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+		std::string rstr = std::to_string(run);
+		std::string output = "../../images/depth" + rstr + ".tga";
+        zbimage.write_tga_file(output.c_str());
+    }
+	
+    delete model;
+    delete [] zbuffer;
 	return 0;
 }
 
